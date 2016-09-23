19747612616308a7eabdee5de1fbbf07cf234528
misaki0729 <popp0729@gmail.com>
2016-09-23 14:59:49 +0900

    楕円の描画, 内部判定まで

--- ellipse/js/canvas.js
+++ ellipse/js/canvas.js
@@ -1,9 +1,9 @@
 var canvas = {
-  canvas_width: 300,
+  canvas_width: 340,
   canvas_height: 300,
-  width: 260,
+  width: 300,
   height: 260,
-  start: 300 / 15,
+  start: 340 / 15,
 };
 
 var canvas_left;
@@ -43,6 +43,22 @@ function init(shape) {
           context_right.strkeStyle = 'rgb(0, 0, 0)';
           context_right.stroke();
           break;
+        case "ellipse":
+        canvas_right = null;
+        context_right = null;
+        context_left.beginPath();
+        context_left.ellipse(canvas.canvas_width/2, canvas.canvas_height/2, canvas.width/2, canvas.width/3, 0, 0, Math.PI*2);
+        context_left.stroke();
+
+        // 焦点
+        var marginX = (canvas.canvas_width - canvas.width)/2;
+        context_left.beginPath();
+        context_left.ellipse(marginX + 2 * canvas.width/10, canvas.canvas_height/2, 2, 2, 0, 0, Math.PI*2);
+        context_left.ellipse(canvas.canvas_width - (marginX + 2 * canvas.width/10), canvas.canvas_height/2, 2, 2, 0, 0, Math.PI*2);
+        context_left.fillStyle = 'rgb(200, 0, 0)';
+        context_left.fill();
+        break;
+          break;
         default:
           return;
       }
@@ -56,6 +72,9 @@ function draw() {
       break;
     case "circle":
       drawCircle();
+      break;
+    case "ellipse":
+      drawEllipse();
       break;
     default:
       return;
@@ -173,6 +192,52 @@ function drawCircle() {
       moveX = Math.cos(rad);
       moveY = Math.sin(rad);
       count++;
+    }
+  }
+}
+
+function drawEllipse() {
+  var start_position = Number(document.getElementById("position").value);
+  var reflection = Number(document.getElementById("reflection").value);
+  var angle = Number(document.getElementById("angle").value);
+
+  if (start_position <= 0 || start_position >= 1) {
+    window.alert("出発位置を入力し直してください。");
+    return false;
+  }
+  if (reflection <= 0) {
+    window.alert("反射回数を入力し直してください。");
+    return false;
+  }
+  if (angle <= 0 || angle >= 180) {
+    window.alert("出発角度を入力し直してください。");
+    return false;
+  }
+
+  var count = 0;
+  var startAngle = angle;
+  var marginX = (canvas.canvas_width - canvas.width)/2;
+  var marginY = canvas.canvas_height/2 - canvas.width/3;
+  var posX = marginX + start_position * canvas.width;
+  var posY = canvas.canvas_height/2;
+  var rad = angle/180 * Math.PI;
+  var moveX = Math.cos(rad);
+  var moveY = Math.sin(rad);
+
+  while(true) {
+    if (count > reflection) {
+      break;
+    }
+
+    posX += moveX;
+    posY -= moveY;
+    context_left.fillStyle = 'rgb(0, 0, 0)';
+    context_left.fillRect(posX, posY, 1, 1);
+
+    var dist = Math.pow((posX - marginX) - canvas.width/2, 2) / Math.pow(canvas.width/2, 2) + Math.pow((posY - marginY) - canvas.width/3, 2) / Math.pow(canvas.width/3, 2);
+    // 内部判定
+    if (dist >= 1) {
+
     }
   }
 }
